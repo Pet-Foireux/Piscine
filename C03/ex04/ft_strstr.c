d4ea53bcb1e04fe579d080797e95c270a75a3c5e
Marius Papin <mpapin@c1r5p10.42lehavre.fr>
2024-08-12 18:51:50 +0200

    shrek est bien

--- C03/ex04/ft_strstr.c
+++ C03/ex04/ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: mpapin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 16:59:44 by mpapin            #+#    #+#             */
-/*   Updated: 2024/08/12 17:43:32 by mpapin           ###   ########.fr       */
+/*   Updated: 2024/08/12 18:42:04 by mpapin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,10 @@ char *ft_strstr(char *str, char *to_find)
 	while (str[i] != '\0')
 	{
 		i++;
-		if (to_find[j] == str[i])
+		while(to_find[j] == str[i])
 		{
-			return (to_find);
-		}
+			to_find++;
+		}return (&str[i]);
 	}return ("NULL");
 
 }
@@ -35,6 +35,6 @@ char *ft_strstr(char *str, char *to_find)
 int	main(void)
 {
 	char str[]="shrek et fiona sont le couple goal";
-	char to_find[]="le";
+	char to_find[]="et";
 	printf("%s", ft_strstr(str, to_find));
 }
