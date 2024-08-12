7ecd6e142ba127f1bab86914ded6b9e6da67d0bd
PetFoireux <pet-foireux@oo>
2024-08-12 22:09:45 +0200

    qui est plus fort que teddy rinner : shrek

--- C03/ex04/ft_strstr.c
+++ C03/ex04/ft_strstr.c
@@ -14,40 +14,39 @@
 
 #include <unistd.h>
 
-char    *ft_strstr(char *str, char *to_find)
+char	*ft_strstr(char *str, char *to_find)
 {
-        int     i;
-        int     j;
+	int	i;
+	int	j;
 
-        if (*to_find == '\0')
-	{
+	if (*to_find == '\0')
 		return (str);
-	}
-        while (*str != '\0')
-        {
-                if (*str == *to_find)
-                {
-                        i = 0;
-                        j = 0;
-                        while (to_find[i] != '\0')
-                        {
-                                if (str[i] != to_find[i])
-                                        j = 1;
-                                i++;
-                        }
-                        if (j == 0)
-                        {
-				return (str);
+	while (*str != '\0')
+	{
+		if (*str == *to_find)
+		{
+			i = 0;
+			j = 0;
+			while (to_find[i] != '\0')
+			{
+				if (str[i] != to_find[i])
+				{
+					j = 1;
+					i++;
+				}
 			}
-                }
-                str++;
-        }
-        return (0);
+			if (j == 0)
+				return (str);
+		}
+		str++;
+	}
+	return (0);
 }
-
+/*
 int	main(void)
 {
 	char str[]="shrek et fiona sont le couple goal";
 	char to_find[]="go";
 	printf("%s", ft_strstr(str, to_find));
 }
+*/
