749ddb6af9a96f488c13bfe951c825e02dd1eb92
PetFoireux <pet-foireux@oo>
2024-08-15 17:46:06 +0200

    mechanttttte

---
+++ C04/ex03/ft_atoi.c
@@ -0,0 +1,36 @@
+#include <unistd.h>
+
+#include <stdio.h>
+
+int	ft_atoi(char *str)
+{
+	int	result;
+	int	sign;
+	int	i;
+
+	result = 0;
+	sign = 1;
+	i = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -sign;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (sign * result);
+}
+/*
+int	main(void)
+{
+	char	str1[] = "   ---+--+1234ab567";
+	printf("%d", ft_atoi(str1));
+	return (0);
+}
+*/
