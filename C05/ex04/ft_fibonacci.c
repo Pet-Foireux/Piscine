930f3caffc69a1beca54863f2bba7ce2730b9946
Marius Papin <mpapin@c1r5p10.42lehavre.fr>
2024-08-19 13:54:39 +0200

    shrek is god

---
+++ C05/ex04/ft_fibonacci.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpapin <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/19 10:27:25 by mpapin            #+#    #+#             */
+/*   Updated: 2024/08/19 10:45:19 by mpapin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+int	ft_fibonacci(int index)
+{
+	if (index < 0)
+		return (-1);
+	if (index == 0)
+		return (0);
+	if (index == 1)
+		return (1);
+	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
+/*
+int	main(void)
+{
+	int	index;
+
+	index = 200;
+	printf("%d", ft_fibonacci(index));
+	return (0);
+}
+*/
