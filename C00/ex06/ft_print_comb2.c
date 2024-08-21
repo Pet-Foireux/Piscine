1e1d006f6bc24e5f7328a449f8eb84da214f007e
Marius Papin <mpapin@c1r5p10.42lehavre.fr>
2024-08-21 13:34:26 +0200

    shrek is meeeee

--- C00/ex06/ft_print_comb2.c
+++ C00/ex06/ft_print_comb2.c
@@ -1,13 +1,53 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mpapin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/21 10:45:07 by mpapin            #+#    #+#             */
-/*   Updated: 2024/08/21 10:45:19 by mpapin           ###   ########.fr       */
+/*   Created: 2024/08/07 17:48:51 by mpapin            #+#    #+#             */
+/*   Updated: 2024/08/21 13:31:15 by mpapin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	affiche(int n)
+{
+	ft_putchar(n / 10 + '0');
+	ft_putchar(n % 10 + '0');
+}
+
+void	ft_print_comb2(void)
+{
+	int	a;
+	int	b;
+
+	a = 0;
+	b = 1;
+	while (a <= 98)
+	{
+		while (b <= 99)
+		{
+			affiche(a);
+			ft_putchar(' ');
+			affiche(b);
+			if (a != 98)
+				write(1, ", ", 2);
+			b++;
+		}
+		a++;
+		b = a + 1;
+	}
+}
+
+int	main(void)
+{
+	ft_print_comb2();
+	return (0);
+}
