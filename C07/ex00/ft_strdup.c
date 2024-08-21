08fddd4db485e49a3cb17ad27358bd6c09c511c4
Marius Papin <mpapin@c1r5p10.42lehavre.fr>
2024-08-21 17:31:11 +0200

    shrek is life, shrek is love

--- C07/ex00/ft_strdup.c
+++ C07/ex00/ft_strdup.c
@@ -6,8 +6,41 @@
 /*   By: mpapin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/21 10:45:07 by mpapin            #+#    #+#             */
-/*   Updated: 2024/08/21 10:45:19 by mpapin           ###   ########.fr       */
+/*   Updated: 2024/08/21 17:30:30 by mpapin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*result;
+	int		i;
+
+	i = 0;
+	result = (char *)(malloc(sizeof(char) * ft_strlen(src)));
+	while (src[i])
+	{
+		result[i] = src[i];
+		i++;
+	}
+	return (result);
+}
+
+int	main()
+{
+	printf("%s\n", ft_strdup("Shrek"));
+	return 0;
+}
