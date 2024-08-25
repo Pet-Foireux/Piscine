d00944957591d37750adb36d64cd3f3497a3d1a2
PetFoireux <pet-foireux@oo>
2024-08-25 22:53:42 +0200

    shrek si the best

---
+++ C07/ex01/ft_range.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int	*ft_range(int min, int max)
+{
+	int	*arr;
+	int	i;
+
+	if (min >= max)
+		return (NULL);
+	arr = malloc(sizeof(int) * (max - min));
+	if (arr == NULL)
+		return (NULL);
+	i = 1;
+	while (min < max)
+	{
+		arr[i] = min;
+		min++;
+		i++;
+	}
+	return (arr);
+}
+
+int	main(void)
+{
+	int	*arr;
+	int	i;
+
+	arr = ft_range(1, 50);
+	i = 1;
+	while (i < 50)
+	{
+		printf("%d\n", arr[i]);
+		i++;
+	}
+	free(arr);
+	return (0);
+}
---
+++ C07/ex02/ft_ultimate_range.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int	ft_ultimate_range(int **range, int min, int max)
+{
+	int	i;
+
+	if (min >= max)
+	{
+		*range = NULL;
+		return (0);
+	}
+	*range = malloc(sizeof(int) * (max - min));
+	if (*range == NULL)
+		return (-1);
+	i = 0;
+	while (min < max)
+	{
+		(*range)[i] = min;
+		min++;
+		i++;
+	}
+	return (i);
+}
+
+int	main(void)
+{
+	int	*arr;
+	int	i;
+
+	i = ft_ultimate_range(&arr, 1, 10);
+	printf("size: %d\n", i);
+	i = 0;
+	while (i < 9)
+	{
+		printf("%d\n", arr[i]);
+		i++;
+	}
+	free(arr);
+	return (0);
+}
